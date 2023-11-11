6b84b2c72cf7d917da605b6e881e761d72ce50e8
Joshua <jcal@itu.dk>
2023-11-11 17:30:33 +0100

    Init project

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net"
 
+	pb "github.com/Josh0104/distributed-mutual/proto"
+)
+
+type myInvoicerServer struct {
+	pb.UnimplementedInvoicerServer
+}
+
+func (s myInvoicerServer) Create(ctx context.Context, req *pb.CreateRequest) (*pb.CreateResponse, error) {
+	return &pb.CreateResponse{
+		Pdf:  []byte(req.From),
+		Docx: []byte("test"),
+	}, nil
+}
 func main() {
 	fmt.Println("Hello, World!")
+	lis, err := net.Listen("tcp", ":8089")
+	if err != nil {
+		log.Fatalf("cannot create listener: %s", err)
+	}
+	serverRegistrar := grpc.NewServer()
+	service := &myInvoicerServer{}
+	pb.RegisterInvoicerServer(serverRegistrar, service)
+	err = serverRegistrar.Serve(lis)
+	if err != nil {
+		log.Fatalf("impossible to serve: %s", err)
+	}
 }
+
+
