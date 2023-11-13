3d87024e0d5b7323eb9d778ff0ad9e720bda0002
Joshua <jcal@itu.dk>
2023-11-13 13:45:52 +0100

    Refactor code

--- client.go
+++ client.go
@@ -3,25 +3,65 @@ package main
 import (
 	"context"
 	"log"
+	"sync"
+	// "io"
 )
 
+var (
+	wg sync.WaitGroup
+)
 
-
-func StreamFromClient(client MutualServiceClient, port_ string) {
-	log.Printf("Client %s is streaming to server", port_)
+func StreamFromClient(client MutualServiceClient, message string) {
 	// Create a stream by calling the streaming RPC method
-	log.Printf("Client %s is streaming to server", addr)
+	log.Printf(message)
 	stream, err := client.StreamFromClient(context.Background())
 	if err != nil {
 		log.Fatalf("could not get stream: %v", err)
 	}
 
 	// Send a message to the server using the stream
-	err = stream.Send(&Message{Content: "Hello from client", SenderId: "client" + addr})
+	err = stream.Send(&Message{Content: message, SenderId: "client" + addr})
 	if err != nil {
 		log.Fatalf("could not send message to server: %v", err)
 	}
 
-	// ... Continue interacting with the stream as needed.
-	// You can send more messages or receive responses from the server.
 }
+
+
+// func (s *MutualService) StreamFromServer(req *Message, stream MutualService_StreamFromServerServer) error {
+// 	for {
+// 		// get the next message from the stream
+// 		err := stream.Send(req)
+ 
+// 		// the stream is closed so we can exit the loop
+// 		if err == io.EOF {
+// 			break
+// 		}
+// 		// some other error
+// 		if err != nil {
+// 			return err
+// 		}
+// 		// log the message
+// 		// log.Printf("Received message from %s: %s", msg.SenderId, msg.Content)
+// 	}
+// 	return nil
+// }
+
+// func (s *MutualService) StreamFromClient(msgStream MutualService_StreamFromClientServer) error {
+// 	for {
+// 		// get the next message from the stream
+// 		msg, err := msgStream.Recv()
+
+// 		// the stream is closed so we can exit the loop
+// 		if err == io.EOF {
+// 			break
+// 		}
+// 		// some other error
+// 		if err != nil {
+// 			return err
+// 		}
+// 		// log the message
+// 		log.Printf("Received message from %s: %s", msg.SenderId, msg.Content)
+// 	}
+// 	return nil
+// }
--- main.go
+++ main.go
@@ -18,10 +18,14 @@ type MutualService struct {
 	UnimplementedMutualServiceServer
 	name string // Not required but useful if you want to name your server
 	port string // Not required but useful if your server needs to know what port it's listening to
+	clientMu sync.Mutex
 
-	incrementValue int64      // value that clients can increment.
-	mutex          sync.Mutex // used to lock the server to avoid race conditions.
 }
+
+type _BroadcastSubscription struct {
+	stream MutualService_BroadcastServer
+}
+
 
 var (
 	port_numbers       = [3]string{":5001", ":5002", ":5003"}
@@ -32,11 +36,13 @@ var (
 	wgServer           sync.WaitGroup
 	wgClients          sync.WaitGroup
 	globalCancel       context.CancelFunc // Declare at a higher scope
-	clients_server     MutualServiceClient
 	mutual_client      MutualServiceClient //the server
 	mutual_server      MutualServiceServer //the client
-
 )
+
+type Config struct {
+	ClientMaps map[string]_BroadcastSubscription
+}
 
 func main() {
 	flag.Parse()
@@ -85,6 +91,7 @@ func startServer() {
 		name: "client" + addr,
 		port: addr,
 	}
+
 	RegisterMutualServiceServer(serverRegistrar, service)
 	err = serverRegistrar.Serve(lis)
 	if err != nil {
@@ -130,28 +137,30 @@ func promptForInput() {
 			active = false
 		} else if input == "Send message to clients" {
 			log.Printf("Sending message to other clients")
-		// 	if input == "chat" {
-		// 		if active {
-		// 			prompt := promptui.Prompt{
-		// 				Label: "input your message and send ",
-		// 				Validate: func(input string) error {
-		// 					if len(input) == 0 || len(input) > 128 {
-		// 						return fmt.Errorf("input must be at least 1 character or under 128 characters")
-		// 					}
-		// 					return nil
-		// 				},
-		// 			}
-		// 			input, err := prompt.Run()
-		// 			if err != nil {
-		// 				log.Fatalf("could not get input: %v", err)
-		// 			}
+			if input == "Send message to clients" {
+				if active {
+					prompt := promptui.Prompt{
+						Label: "input your message and send ",
+						Validate: func(input string) error {
+							if len(input) == 0 || len(input) > 128 {
+								return fmt.Errorf("input must be at least 1 character or under 128 characters")
+							}
+							return nil
+						},
+					}
+					input, err := prompt.Run()
+					if err != nil {
+						log.Fatalf("could not get input: %v", err)
+					}
 	
-		// 			send(client, input)
-		// 		}
-		// 	}
-		// } else {
-		// 	log.Fatalf("Invalid input")
-		// }
+					// sendMessageToClients(otherClientsServer[0], input)
+					// sendMessageToClients(otherClientsServer[1], input)	
+					send(otherClientsServer[0], input)
+					send(otherClientsServer[1], input)		
+				}
+			}
+		} else {
+			log.Fatalf("Invalid input")
 		}
 
 	}
@@ -159,7 +168,10 @@ func promptForInput() {
 
 func enterCriticalSection(name string) {
 	defer leaveCriticalSection(name)
-	log.Printf("Client %s entering critical section", name)
+	log.Printf("Client %s entering critical section", addr)
+	var message = "Client "+addr+" entering critical section"
+	StreamFromClient(otherClientsServer[0], message)
+	StreamFromClient(otherClientsServer[1], message)
 }
 
 func leaveCriticalSection(name string) {
@@ -174,23 +186,25 @@ func connectToOtherClients() {
 		go func(port string) {
 			defer wgClients.Done()
 
+			log.Println("Connecting to ", port)
+
 			// Create a gRPC client to interact with the server
 			conn, err := grpc.Dial(fmt.Sprintf("%s", port), grpc.WithInsecure())
 			if err != nil {
 				log.Printf("Failed to dial %s: %v", port, err)
 				return
 			}
-			defer conn.Close()
+			// defer conn.Close()
 
 			// Initialize the gRPC client
 			clients_server := NewMutualServiceClient(conn)
 
 			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 			defer cancel()
-			StreamFromClient(clients_server, port)
+			// StreamFromClient(clients_server, port)
 
 			otherClientsServer = append(otherClientsServer, clients_server)
-			sayHiToClient(clients_server, port)
+			// sayHiToClient(clients_server, port)
 
 			res, err := clients_server.Join(ctx, &JoinRequest{
 				NodeName: addr,
@@ -259,28 +273,25 @@ func sayHiToClient(server MutualServiceClient, port string) {
 	// log.Println("server says: ", farewell)
 }
 
-func sendMessageToClients(server MutualServiceClient, bsv MutualService_StreamFromServerServer) {
-	// send message to other clients
-	// log.Printf("Sending message to other clients")
-	// stream, err := server.StreamFromServer(context.Background())
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
+func sendMessageToClients(server MutualServiceClient, input string) {
+	stream, err := server.StreamFromServer(context.Background(), &Message{Content: "Hello from client", SenderId: "client" + addr})
 
-	// stream.Send(&Message{Content: addr, SenderId: "Hi clients"})
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
+	stream.SendMsg(&Message{Content: input, SenderId: "client" + addr})
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 }
 
 //Server receives message from client
-func (s *MutualService) StreamFromClient(msgStream MutualService_StreamFromClientServer) error {
+
+
+func (s *MutualService) SendMessageHello(stream MutualService_StreamFromServerServer) error {
 	for {
 		// get the next message from the stream
-		msg, err := msgStream.Recv()
+		err := stream.Send(&Message{Content: "Hello from client", SenderId: "client"})
 
 		// the stream is closed so we can exit the loop
 		if err == io.EOF {
@@ -291,57 +302,12 @@ func (s *MutualService) StreamFromClient(msgStream MutualService_StreamFromClien
 			return err
 		}
 		// log the message
-		log.Printf("Received message from %s: %s", msg.SenderId, msg.Content)
+		// log.Printf("Received message from %s: %s", msg.SenderId, msg.Content)
 	}
-
 	return nil
 }
 
-// func (s *MutualService) SendMessageHello(stream MutualService_StreamFromServerServer) error {
-// 	for {
-// 		// get the next message from the stream
-// 		err := stream.Send(&Message{Content: "Hello from client", SenderId: "client"})
-
-// 		// the stream is closed so we can exit the loop
-// 		if err == io.EOF {
-// 			break
-// 		}
-// 		// some other error
-// 		if err != nil {
-// 			return err
-// 		}
-// 		// log the message
-// 		// log.Printf("Received message from %s: %s", msg.SenderId, msg.Content)
-// 	}
-// 	return nil
-// }
-
-// func (s *MutualService) StreamFromServer(stream MutualService_StreamFromServerServer) error {
-// 	for {
-// 		// get the next message from the stream
-// 		err := stream.Send(&Message{Content: "Hello from client", SenderId: "client"})
-
-// 		// the stream is closed so we can exit the loop
-// 		if err == io.EOF {
-// 			break
-// 		}
-// 		// some other error
-// 		if err != nil {
-// 			return err
-// 		}
-// 		// log the message
-// 		// log.Printf("Received message from %s: %s", msg.SenderId, msg.Content)
-// 	}
-// 	return nil
-// }
-
-// func send(client MutualServiceClient, input string) {
-// 	// Create a stream by calling the streaming RPC method
-// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-// 	defer cancel()
-
-// 	_, err := client.Send(ctx, &Message{Content: input, SenderId: "client" + addr})
-// 	if err != nil {
-// 		log.Fatalf("could not connect: %v", err)
-// 	}
-// }
+func send(client MutualServiceClient, input string) {
+	// Create a stream by calling the streaming RPC method
+	StreamFromClient(client, input)
+}
